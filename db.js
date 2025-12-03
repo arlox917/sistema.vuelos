47e0e6e4f0c93021befc774e1a53efcadfca4180
arlox917 <olmosalejandra867@gmail.com>
2025-12-03 09:54:58 -0600

    Refactor db.js for environment variable support
    Updated database connection configuration to use environment variables and added SSL options.

--- db.js
+++ db.js
@@ -1,10 +1,23 @@
-const mysql = require('mysql2');
+const mysql = require("mysql2");
 
 const db = mysql.createPool({
-  host: 'localhost',
-  user: 'admin',
-  password: 'admin',
-  database: 'flightdb'
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+db.getConnection((err, connection) => {
+  if (err) {
+    console.error("❌ Error conectando a MySQL:", err);
+  } else {
+    console.log("✅ MySQL conectado correctamente (Aiven)");
+    connection.release();
+  }
 });
 
 module.exports = db;
